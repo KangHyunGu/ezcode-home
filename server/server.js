fc9973686fc5a42696bc128f8b2192503808b869
KangHyunGu <gusrn45@naver.com>
2021-07-03 15:31:00 +0900

    20210703-1

--- server/server.js
+++ server/server.js
@@ -20,4 +20,4 @@ webServer.listen(port, () => {
 // 함수 람다식 표현
 // 함수 정의 : test() => {}
 // 파라미터 1 함수 내 1줄의 리턴일 경우 아래와 같이 축약 가능
-// const test = param1 => param1 + 1000;
+// const test = param1 => param1 + 1000;'
