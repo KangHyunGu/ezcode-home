52f2e1d003fa604f1116f34cc51d4b18406d25c4
KangHyunGu <gusrn45@naver.com>
2021-07-03 18:46:52 +0900

    SSR 환경설정

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export function createStore() {
+  const store = new Vuex.Store({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+  })
+  return store;
+}
+
